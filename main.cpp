fa31e0332c61415c7f2f00176cbd711511d48412
kunaltawatia <tawatia.1@iitj.ac.in>
2019-11-26 14:15:49 +0530

    v0.0.1 (Bugs)

--- main.cpp
+++ main.cpp
@@ -3,22 +3,23 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
+#include <unistd.h>
 
 #define clear() printf("\033[H\033[J")
-
-#include <stdlib.h>
-#include <ios>
-#include <limits>
-
-#include <termios.h>
-#include <unistd.h>
-#include <cstdlib>
-
-using namespace std;
-int level = 0, mode = 0, valid_action_available = 1;
 #define STUDENT 1
 #define CAPTAIN 2
 #define f(i, n, m) for (int i = n; i < m; i++)
+using namespace std;
+
+int level = 0, mode = 0;
+template <typename T>
+bool find(vector<T> v, T elem)
+{
+	bool flag = 0;
+	for (auto e : v)
+		flag |= elem == e;
+	return flag;
+}
 
 class Student
 {
@@ -70,6 +71,25 @@ public:
 		cout << name << endl
 			 << rollNo << endl
 			 << ldap << endl;
+		cout << endl;
+	}
+	void requestItem(int item)
+	{
+		ofstream fout("request_transactions.txt", ios::app);
+		fout << rollNo << " REQUEST_ITEM " << item << endl;
+		fout.close();
+	}
+	void requestItemReturn(int item)
+	{
+		ofstream fout("request_transactions.txt", ios::app);
+		fout << rollNo << " REQUEST_ITEM_RETURN " << item << endl;
+		fout.close();
+	}
+	void requestMembership(string club)
+	{
+		ofstream fout("request_transactions.txt", ios::app);
+		fout << rollNo << " REQUEST_MEMBERSHIP " << club << endl;
+		fout.close();
 	}
 } stu;
 class Captain : public Student
@@ -130,6 +150,7 @@ public:
 	{
 		cout << societyName << endl;
 		f(i, 0, clubs.size()) cout << i + 1 << ". " << clubs[i] << endl;
+		cout << endl;
 	}
 } s;
 
@@ -170,17 +191,15 @@ public:
 		cout << itemID << " " << name << endl;
 		if (status == "ISSUED")
 		{
+
 			cout << "ISSUED BY:\n";
 			issuedBy.display();
-			if (stu.rollNo == issuedBy.rollNo)
-				cout << "RETURN? [y/n] ";
-			else
-				valid_action_available = 0;
 		}
 		else
 		{
-			cout << "REQUEST ISSUE? [y/n] ";
+			cout << status << endl;
 		}
+		cout << endl;
 	}
 } it;
 
@@ -242,6 +261,7 @@ public:
 		{
 			cout << itemsAvailable[i].itemID << " " << itemsAvailable[i].name << " " << (itemsAvailable[i].status == "ISSUED" ? ":NOT AVAILABLE" : ":AVAILABLE") << endl;
 		}
+		cout << endl;
 	}
 	void newmember(string roll)
 	{
@@ -275,21 +295,138 @@ public:
 	}
 } c;
 
-class Receipt
+ifstream transaction_fin;
+
+class transaction
 {
 public:
-	static int receiptNo;
 	string studentRollNo;
+	Student student;
+	string request;
 	int itemID;
-	string dateIssued;
-	string dueDate;
-	void createReceipt();
-	void deleteReceipt();
-	void retrieveReceipt();
+	Item item;
+	string clubName;
+	int load()
+	{
+		itemID = 0;
+		if (transaction_fin >> studentRollNo)
+			;
+		else
+			return 1;
+		student.load(studentRollNo);
+		transaction_fin >> request;
+		if (request == "REQUEST_ITEM_RETURN" || request == "REQUEST_ITEM")
+		{
+			transaction_fin >> itemID;
+			item.input(itemID);
+		}
+		else
+		{
+			getline(transaction_fin, clubName);
+			clubName = clubName.substr(1);
+		}
+		return 0;
+	}
+	void display()
+	{
+		cout << request << endl
+			 << endl;
+		student.display();
+		if (request == "REQUEST_ITEM_RETURN" || request == "REQUEST_ITEM")
+		{
+			item.display();
+		}
+		cout << endl
+			 << signature() << endl
+			 << endl;
+	}
+
+	string signature()
+	{
+		if (itemID)
+			return studentRollNo + " " + request + " " + to_string(itemID);
+		else
+			return studentRollNo + " " + request + " " + clubName;
+	}
+
+	bool relative()
+	{
+		bool flag = 0;
+		if (itemID)
+			for (auto item : c.itemsAvailable)
+				flag |= item.itemID == itemID;
+		else
+			flag = c.clubName == clubName;
+		return flag;
+	}
 };
+
+void write(vector<string> confirm, vector<string> skipped)
+{
+	ofstream fout("request_transactions.txt");
+	for (auto sig : skipped)
+		fout << sig << endl;
+	fout.close();
+	fout.open("confirm_transactions.txt", ios::app);
+	for (auto sig : confirm)
+		fout << sig << endl;
+	fout.close();
+}
+
+void transactions()
+{
+	string response;
+	transaction_fin.open("request_transactions.txt");
+	vector<string> confirm_ts, skipped_ts;
+	transaction t;
+	int exitStatus;
+label:
+	exitStatus = t.load();
+	if (exitStatus)
+	{
+		transaction_fin.close();
+		write(confirm_ts, skipped_ts);
+		return;
+	}
+	if (!t.relative())
+	{
+		skipped_ts.push_back(t.signature());
+		goto label;
+	}
+	clear();
+	t.display();
+	cout << "1. ACCEPT\n2. DECLINE\n3. NEXT REQUEST\n";
+	cout << "ENTER INDEX (0 FOR EXIT)\n";
+	cin >> response;
+	switch (response[0])
+	{
+	case '0':
+		break;
+	case '1':
+		confirm_ts.push_back(t.signature());
+		goto label;
+	case '2':
+		goto label;
+	case '3':
+		skipped_ts.push_back(t.signature());
+		goto label;
+	default:
+		break;
+	}
+	while (!exitStatus)
+	{
+		skipped_ts.push_back(t.signature());
+		exitStatus = t.load();
+	}
+	transaction_fin.close();
+	write(confirm_ts, skipped_ts);
+	return;
+}
 
 int loadStudent()
 {
+	stu.input("tawatia.1", "pass");
+	return 0;
 	string ldap, pass;
 	cout << "Enter LDAP:";
 	cin >> ldap;
@@ -310,11 +447,15 @@ int loadSociety()
 	getline(fin, dump);
 	vector<string> societies(total);
 	f(i, 0, total) getline(fin, societies[i]);
+
+	cout << "Student Gymkhana\n\n";
 	f(i, 0, total) cout << i + 1 << ". " << societies[i] << endl;
+	cout << endl;
 
 	int num;
 	cout << "Enter Index: (0 for back)\n";
 	cin >> num;
+	fflush(stdin);
 	if (num == 0)
 	{
 		level -= 2;
@@ -332,6 +473,7 @@ int loadClub()
 	int num;
 	cout << "Enter Index: (0 for back)\n";
 	cin >> num;
+	fflush(stdin);
 	if (num == 0)
 	{
 		level -= 2;
@@ -348,6 +490,7 @@ int loadItem()
 	int num;
 	cout << "Enter Index: (0 for back)\n";
 	cin >> num;
+	fflush(stdin);
 	if (num == 0)
 	{
 		level -= 2;
@@ -364,19 +507,97 @@ int loadItem()
 
 int loadAction()
 {
-	if (valid_action_available == 0)
+	string response;
+	if (it.status == "ISSUED")
 	{
-		level -= 2;
+		if (stu.rollNo == it.issuedBy.rollNo)
+		{
+			cout << "RETURN? [y/n] ";
+			cin >> response;
+			if (response == "n" || response == "y")
+				level--;
+			if (response == "y")
+				stu.requestItemReturn(it.itemID);
+			return 0;
+		}
+		else
+		{
+			cout << "ENTER 0 TO GO BACK\n";
+			cin >> response;
+			level--;
+			return 0;
+		}
+	}
+	else if (find(c.members, stu.rollNo))
+	{
+		cout << "REQUEST ISSUE? [y/n] ";
+		cin >> response;
+		if (response == "n" || response == "y")
+			level--;
+		if (response == "y")
+			stu.requestItem(it.itemID);
 		return 0;
 	}
-	char num;
-	cin >> num;
-	if (num != 'y')
+	else
 	{
-		level -= 2;
+		cout << "YOU ARE NOT A MEMBER OF THIS CLUB YET\n";
+		cout << "REQUEST MEMBERSHIP? [y/n] ";
+		cin >> response;
+		if (response == "n" || response == "y")
+			level--;
+		if (response == "y")
+			stu.requestMembership(c.clubName);
 		return 0;
 	}
-	return 1;
+}
+
+int loadCaptainActions()
+{
+	int option;
+	string response;
+
+	if (c.captain != stu.rollNo)
+	{
+		cout << "You are not the captain of " << c.clubName << endl;
+		cout << "ENTER 0 TO GO BACK\n";
+		cin >> response;
+		return 1;
+	}
+	cout << "1. SHOW TRANSACTION REQUESTS\n2. SHOW MEMBERS\n3. SHOW ITEMS\n";
+	cout << "ENTER INDEX (0 for back):\n";
+
+	cin >> option;
+	fflush(stdin);
+	switch (option)
+	{
+	case 0:
+		return 1;
+	case 1:
+		transactions();
+		return 0;
+	case 2:
+		clear();
+		for (auto member : c.members)
+		{
+			Student s;
+			s.load(member);
+			s.display();
+		}
+		cout << "ENTER 0 TO GO BACK\n";
+		cin >> response;
+		return 0;
+	case 3:
+		clear();
+		for (auto item : c.itemsAvailable)
+		{
+			item.display();
+		}
+		cout << "ENTER 0 TO GO BACK\n";
+		cin >> response;
+		return 0;
+	default:
+		return 0;
+	}
 }
 
 void EXIT(int input)
@@ -393,7 +614,7 @@ void display()
 	switch (level)
 	{
 	case 0:
-		cout << "1. Student\n2. Captain\n3. Exit\n";
+		cout << "1. Student\n2. Captain\n3. Exit\n\n";
 		cin >> mode;
 		if (!(mode == 1 || mode == 2))
 			EXIT(0);
@@ -417,18 +638,26 @@ void display()
 		level++;
 		return;
 	case 3:
-		c.display();
-		exitStatus = loadItem();
-		if (exitStatus)
-			EXIT(0);
-		level++;
+		if (mode == STUDENT)
+		{
+			c.display();
+			exitStatus = loadItem();
+			if (exitStatus)
+				EXIT(0);
+			level++;
+		}
+		else
+		{
+			exitStatus = loadCaptainActions();
+			if (exitStatus)
+				EXIT(0);
+		}
 		return;
 	case 4:
 		it.display();
 		exitStatus = loadAction();
 		if (exitStatus)
 			EXIT(0);
-		level++;
 		return;
 	default:
 		EXIT(0);
